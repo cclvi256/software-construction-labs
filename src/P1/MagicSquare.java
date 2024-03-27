14da46750a5167043b115205f4e39855cc44561c
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-03-27 20:34:08 +0800

    a commit before leaving

--- src/P1/MagicSquare.java
+++ src/P1/MagicSquare.java
@@ -1,7 +1,44 @@
 package P1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
 public class MagicSquare {
   public static void main(String[] args) {
-
+  
+  }
+  
+  private static String getContent(String path) {
+    try {
+      return Files.readString(Paths.get(path));
+    } catch (IOException e1) {
+      System.out.println("File not found or corrupted.");
+      System.exit(1);
+    } catch (InvalidPathException e2) {
+      System.out.println("Invalid path.");
+      System.exit(1);
+    }
+    return null;
+  }
+  
+  private static boolean check(String fileContent) {
+    
+    
+    return false;
+  }
+  
+  /**
+   * spec: To check if a square provided in the file a legal one.
+   *
+   * @param fileName the file path and name, relative path is recommended.
+   * @return If the input file is a legal Magic Square
+   */
+  private static boolean isLegalMagicSquare(String fileName) {
+    
+    
+    // TODO: Modify the return value.
+    return false;
   }
 }
