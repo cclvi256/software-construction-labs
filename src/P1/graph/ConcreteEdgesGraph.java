e54aac3fef1f6ccf4afbe674a4e69ab337c27050
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-04-21 14:17:49 +0800

    Implemented Edge class

--- src/P1/graph/ConcreteEdgesGraph.java
+++ src/P1/graph/ConcreteEdgesGraph.java
@@ -11,79 +11,116 @@ import java.util.Set;
 
 /**
  * An implementation of Graph.
- * 
+ *
  * <p>PS2 instructions: you MUST use the provided rep.
  */
 public class ConcreteEdgesGraph implements Graph<String> {
-    
-    private final Set<String> vertices = new HashSet<>();
-    private final List<Edge> edges = new ArrayList<>();
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    // TODO toString()
-    
+  
+  private final Set<String> vertices = new HashSet<>();
+  private final List<Edge> edges = new ArrayList<>();
+  
+  // Abstraction function:
+  //   TODO
+  //
+  // Representation invariant:
+  //   TODO
+  // Safety from rep exposure:
+  //   TODO
+  
+  // TODO constructor
+  
+  // TODO checkRep
+  
+  @Override
+  public boolean add(String vertex) {
+    throw new RuntimeException("not implemented");
+  }
+  
+  @Override
+  public int set(String source, String target, int weight) {
+    throw new RuntimeException("not implemented");
+  }
+  
+  @Override
+  public boolean remove(String vertex) {
+    throw new RuntimeException("not implemented");
+  }
+  
+  @Override
+  public Set<String> vertices() {
+    throw new RuntimeException("not implemented");
+  }
+  
+  @Override
+  public Map<String, Integer> sources(String target) {
+    throw new RuntimeException("not implemented");
+  }
+  
+  @Override
+  public Map<String, Integer> targets(String source) {
+    throw new RuntimeException("not implemented");
+  }
+  
+  // TODO toString()
+  @Override
+  public String toString() {
+    throw new RuntimeException("not implemented");
+  }
+  
 }
 
 /**
  * TODO specification
  * Immutable.
  * This class is internal to the rep of ConcreteEdgesGraph.
- * 
+ * An Edge is a weighted directed edge from a source vertex to a target vertex.
+ * It can't delete itself, but can change its weight. If deleted, it will be
+ * processed by GC.
+ *
  * <p>PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
 class Edge {
-    
-    // TODO fields
-    
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    // TODO methods
-    
-    // TODO toString()
-    
+  
+  // TODO fields
+  String source;
+  String target;
+  int weight;
+  
+  // Abstraction function:
+  //   An edge from source to target with weight Edge.weight.
+  // Representation invariant:
+  //   weight >= 0
+  //   source != null
+  //   target != null
+  //   source != target
+  //   source and target are in the vertices set of the graph
+  // Safety from rep exposure:
+  //   All fields are private and immutable.
+  
+  // TODO constructor
+  Edge(String source, String target, int weight) {
+    this.source = source;
+    this.target = target;
+    this.weight = weight;
+  }
+  
+  // TODO checkRep
+  
+  boolean checkRep() {
+    return weight >= 0 && source != null && target != null &&
+        !source.equals(target);
+  }
+  
+  // TODO methods
+  
+  Edge setWeight(int weight) {
+    return new Edge(source, target, weight);
+  }
+  
+  // TODO toString()
+  @Override
+  public String toString() {
+    return source.toString() + '\t' + target.toString() + '\t' + weight;
+  }
 }
