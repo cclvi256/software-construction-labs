fd5156c93973fd8e45025b8c88169445f3aef05a
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-04-25 14:26:12 +0800

    Implemented methods in SocialNetwork

---
+++ test/P2/SocialNetworkTest.java
@@ -0,0 +1,16 @@
+package P2;
+
+import org.junit.Test;
+
+public class SocialNetworkTest {
+  @Test(expected = AssertionError.class)
+  public void testAssertionEnabled() {
+    assert false;
+  }
+  
+  // Testing strategy
+  /*
+  All is the same as the previous one in Lab 1.
+   */
+}
+
