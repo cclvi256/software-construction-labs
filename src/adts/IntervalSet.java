7dec76f427c0a2f69a0fd4f14d567c56fa004545
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-21 23:26:36 +0800

    Separate IntervalTime from IntervalSet.java

--- src/adts/IntervalSet.java
+++ src/adts/IntervalSet.java
@@ -3,24 +3,6 @@ package adts;
 import java.util.List;
 import java.util.Map;
 
-class IntervalTime {
-  private int start;
-  private int end;
-  
-  public IntervalTime(int start, int end) {
-    this.start = start;
-    this.end = end;
-  }
-  
-  public int getStart() {
-    return start;
-  }
-  
-  public int getEnd() {
-    return end;
-  }
-}
-
 public class IntervalSet<T> implements IIntervalSet<T> {
   private Map<T, IntervalTime> intervals;
   
