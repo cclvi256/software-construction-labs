bfff9896bf9aca30a1ce98fd706e2dc90dac8e40
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-22 05:14:50 +0800

    Add tests for interval and all the tests passed

--- src/adt/Interval.java
+++ src/adt/Interval.java
@@ -4,4 +4,26 @@ public class Interval<T> {
   private T label;
   private long begin;
   private long end;
+  
+  Interval(T label, long begin, long end) {
+    this.label = label;
+    this.begin = begin;
+    this.end = end;
+  }
+  
+  public T getLabel() {
+    return label;
+  }
+  
+  public long getBegin() {
+    return begin;
+  }
+  
+  public long getEnd() {
+    return end;
+  }
+  
+  public long getLength() {
+    return end - begin;
+  }
 }
--- test/adt/IntervalTest.java
+++ test/adt/IntervalTest.java
@@ -1,4 +1,31 @@
 package adt;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntervalTest {
+  @Test
+  public void test1() {
+    Interval<String> i = new Interval<>("a", 200410141808L, 200410141809L);
+    assertEquals("a", i.getLabel());
+    assertEquals(200410141808L, i.getBegin());
+    assertEquals(200410141809L, i.getEnd());
+    assertEquals(1, i.getLength());
+  }
+  
+  @Test public void test2() {
+    Interval<Integer> i = new Interval<>(1, 200410141808L, 200410141809L);
+    assertEquals(1, i.getLabel());
+    assertEquals(200410141808L, i.getBegin());
+    assertEquals(200410141809L, i.getEnd());
+    assertEquals(1, i.getLength());
+  }
+  
+  @Test public void test3() {
+    Interval<Double> i = new Interval<>(1.0, 200410141808L, 200410141809L);
+    assertEquals(1.0, i.getLabel());
+    assertEquals(200410141808L, i.getBegin());
+    assertEquals(200410141809L, i.getEnd());
+    assertEquals(1, i.getLength());
+  }
 }
