476f4b440da905156e9a844e6055e10902b76899
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-05-22 21:56:13 +0800

    Add CRD methods of IntervalSet

--- src/adt/IntervalSet.java
+++ src/adt/IntervalSet.java
@@ -1,31 +1,74 @@
 package adt;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class IntervalSet<T> implements IIntervalSet<T> {
+  private Set<Interval<T>> intervals;
+  private boolean valid;
+  
+  IntervalSet() {
+    intervals = new HashSet<>();
+    valid = true;
+  }
   
   @Override
   public boolean insert(Interval<T> interval) {
-    return false;
+    for (Interval<T> i : intervals) {
+      if (i.equals(interval)) {
+        return false;
+      }
+      
+      if (i.getLabel().equals(interval.getLabel())) {
+        if (i.isOverlap(interval)) {
+          valid = false;
+          break;
+        }
+      }
+    }
+    
+    intervals.add(interval);
+    return true;
   }
   
   @Override
   public Set<Interval<T>> getIntervals() {
-    return Set.of();
+    return new HashSet<>(intervals);
   }
   
   @Override
   public Set<T> getLabels() {
-    return Set.of();
+    Set<T> labels = new HashSet<>();
+    for (Interval<T> i : intervals) {
+      labels.add(i.getLabel());
+    }
+    return labels;
   }
   
   @Override
   public boolean remove(T label) {
-    return false;
+    Set<Interval<T>> toRemove = findIntervals(label);
+    
+    if (toRemove.isEmpty()) {
+      return false;
+    }
+    
+    for (Interval<T> i : toRemove) {
+      intervals.remove(i);
+    }
+    
+    return true;
   }
   
   @Override
   public boolean remove(Interval<T> interval) {
+    for (Interval<T> i : intervals) {
+      if (i.equals(interval)) {
+        intervals.remove(i);
+        return true;
+      }
+    }
+    
     return false;
   }
   
